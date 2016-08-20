9e22ca5cad4465a469cad7480c7bcecd4062b741
Rajalakshmivenkat <rajivenkat.ece@gmail.com>
2016-08-20 17:00:33 +0530

    Create work.java

---
+++ work.java
@@ -0,0 +1,17 @@
+ public boolean isHoliday(Date date)
+    {
+        boolean isHoliday = false;
+        
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        
+        if(cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY
+            || cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+        {
+            isHoliday = true;
+        }
+        
+        
+        
+        return isHoliday;
+    }
